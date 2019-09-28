d4a1a6fa586f40a5d90d5099a87e97812808b0fd
Nalam nabid <nabid.cse.buet@gmail.com>
2019-09-29 02:32:45 +0600

    recursion merge sort first stage

--- src/merge_sort/MergeSort.java
+++ src/merge_sort/MergeSort.java
@@ -3,13 +3,13 @@ package merge_sort;
 public class MergeSort {
     void sort(int arr[], int left, int right){
         if(left < right){
-            System.out.println("int middle = (left + right) / 2;");
+            System.out.println("int middle = (left + right) / 2;" + left + " , " + right );
             int middle = (left + right) / 2;
 
-            System.out.println("sort(arr,left, middle);");
+            System.out.println("sort(arr,left, middle); "+ left + " , " + right + " , "+ middle);
             sort(arr,left, middle);
 
-            System.out.println("sort(arr, middle+1, right);");
+            System.out.println("sort(arr, middle+1, right);" + left + " , " + right + " , "+ middle);
             sort(arr, middle+1, right);
         }
     }
