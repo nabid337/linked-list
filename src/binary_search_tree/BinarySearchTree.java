b4c9264b18f614ca9cc0d3e3b8f78d624ea4a640
Nalam nabid <nabid.cse.buet@gmail.com>
2019-10-04 01:15:22 +0600

    delete two case covered

--- src/binary_search_tree/BinarySearchTree.java
+++ src/binary_search_tree/BinarySearchTree.java
@@ -8,55 +8,106 @@ public class BinarySearchTree {
     }
 
     void insert(int value) {
-         root = insertRec(root, value);
+        root = insertRec(root, value);
     }
 
     Node insertRec(Node root, int value) {
 
         if (root == null) {
-           // Node node = new Node(value);
+            // Node node = new Node(value);
             root = new Node(value);
             return root;
         }
 
-         if (root.value > value) {
+        if (root.value > value) {
             root.left = insertRec(root.left, value);
-        }
-        else if (root.value < value) {
+        } else if (root.value < value) {
             root.right = insertRec(root.right, value);
         }
         return root;
     }
-    void inOrderPrint(){
+
+    void inOrderPrint() {
         inOrderPrintRec(root);
     }
 
-    void inOrderPrintRec(Node root){
-        if (root!=null){
+    void inOrderPrintRec(Node root) {
+        if (root != null) {
             inOrderPrintRec(root.left);
             System.out.println(root.value);
             inOrderPrintRec(root.right);
         }
     }
 
-    boolean search(int value){
-        Node node =  searchValueRec(root, value);
-        if(node==null) return false;
+    boolean search(int value) {
+        Node node = searchValueRec(root, value);
+        if (node == null) return false;
         else return true;
     }
-    Node searchValueRec(Node root, int value){
-        if(root ==null || root.value == value) return root;
-         if(root.value > value) return searchValueRec(root.left, value);
-        else return searchValueRec(root.right, value);
+
+    Node searchValueRec(Node current, int value) {
+        if (current == null || current.value == value) return current;
+        if (current.value > value) return searchValueRec(current.left, value);
+        else return searchValueRec(current.right, value);
     }
 
-    Node getRoot(){
+    Node getRoot() {
         return root;
     }
 
-    boolean deleteNode(int value){
-        return false;
+    /*void deleteNode(int value) {
+        deleteNodeRec(root, value);
     }
 
+    void deleteNodeRec(Node current, int value) {
+        if (current == null) {
+            System.out.println("cant delete");
+        }
+        else if (current.right!=null && current.right.value == value && current.right.left == null && current.right.right ==null) {
+            current.right = null;
+            System.out.println("successfully deleted");
+        }
+        else if (current.left!=null && current.left.value == value && current.left.left == null && current.left.right ==null){
+            current.left = null;
+            System.out.println("successfully deleted");
+        }
+        else if (current.value > value) deleteNodeRec(current.left, value);
+        else deleteNodeRec(current.right, value);
+    }*/
+    public void delete(int value) {
+        root = deleteRecursive(root, value);
+    }
+    private Node deleteRecursive(Node current, int value) {
+        if (current == null) {
+            return null;
+        }
+
+        if (value == current.value) {
+            // Node to delete found
+            // ... code to delete the node will go here
+            if (current.left == null && current.right == null) {
+                return null;
+            }
+            else if (current.left!=null && current.right == null){
+                return current.left;
+            }
+            else if(current.right!=null && current.left == null){
+                return current.right;
+            }
+            else {
+
+            }
+
+            //}
+        }
+        else if (value < current.value) {
+            current.left = deleteRecursive(current.left, value);
+            return current;
+        }
+
+         current.right = deleteRecursive(current.right, value);
+          return current;
+
+    }
 
 }
--- src/binary_search_tree/Main.java
+++ src/binary_search_tree/Main.java
@@ -4,17 +4,28 @@ public class Main {
     public static void main(String args[]){
 
         BinarySearchTree tree = new BinarySearchTree();
-        tree.insert(10);
-        tree.insert(20);
-        tree.insert(30);
-        tree.insert(40);
         tree.insert(50);
+        tree.insert(30);
+        tree.insert(15);
+        //tree.insert(45);
         tree.insert(60);
         tree.insert(70);
+        tree.insert(55);
         tree.inOrderPrint();
-        System.out.println(tree.search(505));
-        System.out.println(tree.getRoot().value);
-       // System.out.println("fdsa");
+        System.out.println("fdasf");
+       // System.out.println(tree.search(45));
+        //System.out.println(tree.getRoot().value);
+        tree.delete(30);
+        tree.delete(55);
+
+        tree.inOrderPrint();
+        System.out.println("fdasf");
+
+        tree.delete(70);
+
+        tree.inOrderPrint();
+        System.out.println("fdasf");
+       // System.out.println(tree.search(45));
 
     }
 }
